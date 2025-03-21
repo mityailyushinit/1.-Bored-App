a6ebbe86ab8fe9a6e6e1a9c72135007e51f4612c
mityailyushinit <mitya.ilyushin1996@inbox.ru>
2025-03-21 12:27:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 fetch("https://dog.ceo/api/breeds/image/random")
-  .then(response => response.json())
-  .then(res => console.log(res));
+  .then(data => data.json())
+  .then((res) => {
+    if(res.status !== 'success') {
+        return;
+    }
+    
+    document.querySelector('.js-img').innerHTML = `
+        <img 
+            src='${res.message}'
+            width='500'>
+    `;
+  });
